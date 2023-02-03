f20dedc6bd5bb188ee46270dc4176417376c1df2
Lukas Paluzga <sajagi@freya.cz>
2023-02-03 13:49:39 +0100

    operators

--- 02_Functions.fs
+++ 02_Functions.fs
@@ -1,5 +1,7 @@
 ﻿module fsharp_demo.Functions
 
+open System
+
 // 2. FUNKCE
 
 // každá funkce má alespoň jeden parametr a má vždy návratovou hodnotu...
@@ -30,7 +32,7 @@ let add1 = f 1
 // stejné jako bych napsal
 // let add1 y = f 1 y
 
-// fuj
+// :(
 let add1ThenDoubleThenPrint0 x =
     printfn $"%i{double (add1 x)}"
 
@@ -43,3 +45,11 @@ let add1ThenDoubleThenPrint1 x = x |> add1 |> double |> printfn "%i"
 // >> operátor
 // let (>>) f1 f2 = fun x -> f1 x |> f2
 let add1ThenDoubleThenPrint2 = add1 >> double >> printfn "%i"
+
+// vlastní operátory
+let (@) x y = System.IO.Path.Combine(x, y)
+let path = Environment.CurrentDirectory @ "foo.txt"
+
+// go wild
+let (|<><><>|) x y = x - y
+let z = 10 |<><><>| 2
